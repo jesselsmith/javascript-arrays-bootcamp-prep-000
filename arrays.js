bee1e23630e47d4dd9d08d8918dc404a26164dbd
Jesse SMith <jesse.lee.smith@gmail.com>
2017-12-18 16:40:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,8 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles' ];
 
 function addElementToBeginningOfArray(array, element){
-  array.unshift(element);
-  return array;
+  returnArray = [element, ...array];
+  return returnArray;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
